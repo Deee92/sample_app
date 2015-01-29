2d2882d2d29e9b37c3949c07f1ef87d6d2cf9a91
Deepika Tiwari <deepikatiwari92@gmail.com>
2015-01-29 22:10:02 +0530

    Sign up form, invalid sign up errors and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+  
+  # GET
   def new
+    @user = User.new
   end
+
+  # GET
+  def show
+    @user = User.find(params[:id])
+    # for byebug prompt
+    # debugger
+  end
+
+  # POST
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
